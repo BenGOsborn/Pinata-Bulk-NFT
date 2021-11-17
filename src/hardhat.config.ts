55d9b32f1b1553ec861a7603b4ac85186fcb524e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-18 10:50:17 +1100

    🎉 Update

--- src/hardhat.config.ts
+++ src/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import dotenv from "dotenv";
+dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -14,9 +16,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
     solidity: "0.8.4",
+    local: {
+        url: "https://api.avax-test.network/ext/bc/C/rpc",
+        chainId: 43113,
+        accounts: [PRIVATE_KEY],
+    },
 };
