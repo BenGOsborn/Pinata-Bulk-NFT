89b2dc66a3ac4fc0f377d57c063b56c8ee6d241f
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-18 10:55:29 +1100

    🎉 Update

--- src/contracts/Bulk.sol
+++ src/contracts/Bulk.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Bulk is ERC721 {
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
+}
