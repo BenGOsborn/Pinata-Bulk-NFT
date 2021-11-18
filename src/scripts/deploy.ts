ec04818ec0f4503493826fd62c29a1a1c3b108e6
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-18 14:01:40 +1100

    🎉 Update

--- src/scripts/deploy.ts
+++ src/scripts/deploy.ts
@@ -1,5 +1,4 @@
 import hre from "hardhat";
-import fs from "fs";
 import metadata from "../metadata/metadata.json";
 
 async function main() {
@@ -11,7 +10,7 @@ async function main() {
 
     // Deploy the contract
     await hre.run("compile");
-    const Bulk = await hre.ethers.getContractFactory("Bulk");
+    const Bulk = await hre.ethers.getContractFactory(NAME);
     const bulk = await Bulk.deploy(NAME, SYMBOL, MAX_TOKENS, MINT_FEE);
     await bulk.deployed();
     console.log(`${NAME} deployed to: ${bulk.address}`);
