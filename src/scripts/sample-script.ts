58037211bce3b614e0a43ad147168c90bd9bd866
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-18 13:56:11 +1100

    🎉 Update

--- src/scripts/sample-script.ts
+++ src/scripts/sample-script.ts
@@ -1,32 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+import hre from "hardhat";
+import fs from "fs";
+import metadata from "../metadata/metadata.json";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+    // Declare initialization values
+    const NAME = "Bulk";
+    const SYMBOL = "BLK";
+    const MAX_TOKENS = metadata.metadata.length;
+    const MINT_FEE = 10;
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+    // Deploy the contract
+    await hre.run("compile");
+    const Bulk = await hre.ethers.getContractFactory("Bulk");
+    const bulk = await Bulk.deploy(NAME, SYMBOL, MAX_TOKENS, MINT_FEE);
+    await bulk.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+    console.log(`${NAME} deployed to: ${bulk.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
