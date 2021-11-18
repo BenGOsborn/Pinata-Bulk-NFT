7831df8f43affa8734fba1b2190468d498db8537
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-18 20:27:32 +1100

    🎉 Update

--- src/scripts/deploy.ts
+++ src/scripts/deploy.ts
@@ -17,6 +17,12 @@ async function main() {
     const bulk = await Bulk.deploy(NAME, SYMBOL, MAX_TOKENS, MINT_FEE, BASE_URI);
     await bulk.deployed();
     console.log(`${NAME} deployed to: https://testnet.snowtrace.io/address/${bulk.address}`);
+
+    // Mint an NFT
+    await bulk.mint({ value: MINT_FEE });
+    const tokenId = 0;
+    const tokenURI = await bulk.tokenURI(tokenId);
+    console.log(`Minted token ${tokenId} with tokenURI ${tokenURI}`);
 }
 
 main()
