260150c37515ef76342d6d5514541aa420a80785
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-18 11:18:27 +1100

    🎉 Update

--- src/contracts/Bulk.sol
+++ src/contracts/Bulk.sol
@@ -6,5 +6,33 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Bulk is ERC721 {
-    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
+    using Strings for uint256;
+
+    // Token data
+    uint256 public immutable maxTokens;
+    uint256 public immutable mintFee;
+    string private baseURI;
+    uint256 private tokenId;
+
+    constructor(string memory name_, string memory symbol_, uint256 maxTokens_, uint256 mintFee_, string memory baseURI_) ERC721(name_, symbol_) {
+        // Initialize the token data
+        maxTokens = maxTokens_;
+        mintFee = mintFee_;
+        baseURI = baseURI_;
+    }
+
+    // Base URI for the metadata
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    modifier mintable {
+        require(tokenId + 1 < maxTokens, "Bulk: Max number of tokens has already been reached");
+        require(msg.value >= mintFee, "Bulk: Not enough funds to mint token");
+        _;
+    }
+
+    function mint() external payable mintable {
+        _mint(_msgSender(), tokenId++);
+    }
 }
