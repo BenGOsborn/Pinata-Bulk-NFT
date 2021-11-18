a3fc4850c1fc1c6e81b0da0fef4560a96d155e16
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-18 14:05:05 +1100

    🎉 Update

--- src/scripts/upload.ts
+++ src/scripts/upload.ts
@@ -1,4 +1,13 @@
-async function main() {}
+import pinataSDK from "@pinata/sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function main() {
+    // Initialize pinata
+    const PINATA_API_KEY = process.env.PINATA_API_KEY as string;
+    const PINATA_API_SECRET = process.env.PINATA_API_SECRET as string;
+    const pinata = pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
+}
 
 main()
     .then(() => process.exit(0))
